5415fe59018f14e290819ede0ab187d2f4cc9745
TavWork <workfortav@gmail.com>
2025-05-22 23:39:25 +0530

    infinte scroll chnges
    infinte scroll chnges

--- src/App.js
+++ src/App.js
@@ -6,29 +6,40 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
   pageSize=15;
   apiKey="8db134a542f04ac1a502ffa87c23e49e"
+
+  state = {
+    progess: 5
+  }
+
+  setProgress = (progress) =>{
+    this.setState({progress: progress})
+  }
+
   render() {
     return (
-      <Router>
         <div>
-          <Navbar/>
+        <Router>
+        <Navbar/>
+        <LoadingBar color="#f11946" progress={this.state.progress} height={3} />
           
           <Routes>
-            <Route exact path="/" element = {<News key="home" pageSize={this.pageSize} county="us" category="general" apiKey={this.apiKey}/>}/>
-            <Route exact path="/entertainment" element = {<News key="entertainment" pageSize={this.pageSize} county="us" category="entertainment" apiKey={this.apiKey}/>}/>
-            <Route exact path="/business" element = {<News key="business" pageSize={this.pageSize} county="us" category="business" apiKey={this.apiKey}/>} />
-            <Route exact path="/general" element = {<News key="general" pageSize={this.pageSize} county="us" category="general" apiKey={this.apiKey}/>} />
-            <Route exact path="/health" element = {<News key="health" pageSize={this.pageSize} county="us" category="health" apiKey={this.apiKey}/>}/>
-            <Route exact path="/science" element = {<News key="science" pageSize={this.pageSize} county="us" category="science" apiKey={this.apiKey}/>} />
-            <Route exact path="/sports" element = {<News key="sports" pageSize={this.pageSize} county="us" category="sports" apiKey={this.apiKey}/>} />
-            <Route exact path="/technology" element = {<News key="technology" pageSize={this.pageSize} county="us" category="technology" apiKey={this.apiKey}/>}/>
-        </Routes>
-
+            <Route exact path="/" element = {<News setProgress={this.setProgress} key="home" pageSize={this.pageSize} county="us" category="general" apiKey={this.apiKey}/>}/>
+            <Route exact path="/entertainment" element = {<News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} county="us" category="entertainment" apiKey={this.apiKey}/>}/>
+            <Route exact path="/business" element = {<News setProgress={this.setProgress} key="business" pageSize={this.pageSize} county="us" category="business" apiKey={this.apiKey}/>} />
+            <Route exact path="/general" element = {<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} county="us" category="general" apiKey={this.apiKey}/>} />
+            <Route exact path="/health" element = {<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} county="us" category="health" apiKey={this.apiKey}/>}/>
+            <Route exact path="/science" element = {<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} county="us" category="science" apiKey={this.apiKey}/>} />
+            <Route exact path="/sports" element = {<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} county="us" category="sports" apiKey={this.apiKey}/>} />
+            <Route exact path="/technology" element = {<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} county="us" category="technology" apiKey={this.apiKey}/>}/>
+          </Routes>
+        </Router>
         </div>
-      </Router>
+     
     )
   }
 }
@@ -37,7 +48,7 @@ export default class App extends Component {
 //   return (
 //     <div>
 //       <Navbar/>
-//       <News pageSize={this.pageSize} county="us" category="sports" apiKey={this.apiKey}/>
+//       <News setProgress={this.setProgress} pageSize={this.pageSize} county="us" category="sports" apiKey={this.apiKey}/>
 //     </div>
 //   )
 // }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,14 @@ export class NewsItem extends Component {
     return (
       <div>
           <div className="card" >
+            <div style={{display: 'flex', justifyContent: 'flex-end', right: '0', position: 'absolute'}}>
+              <span className="badge rounded-pill bg-danger" >
+                  {source? source : "Unknown Source"}
+              </span>
+            </div>
             <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}...<span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : '90%', zIndex:'1'}}>
-                  {source? source : "Unknown Source"}
-                </span></h5>
+              <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
               <p className="card-text"><small className="text-muted">By {author? author: "Unknown"} published on {new Date(date).toUTCString()}</small></p>
               <a href={newsUrl} target='_blank' rel="noopener noreferrer" className="btn btn-primary">Read more</a>
